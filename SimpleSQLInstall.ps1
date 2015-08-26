d5d68fc00c4ebe91686b5b8ba7501d2fffd96185
erleonard <erleonard@outlook.com>
2015-08-25 23:40:36 -0400

    simple hotfix install for localhost

---
+++ SimpleSQLInstall.ps1
@@ -0,0 +1,55 @@
+﻿configuration SimpleSQLInstall {
+
+    Import-DscResource -ModuleName xSQLServer,xDisk
+    Node $AllNodes.NodeName
+    {
+        xWaitForDisk InstallDisk
+        {
+            DiskNumber = 1
+            RetryIntervalSec = 60
+            RetryCount = 3
+        }
+        
+        xDisk Installdisk
+        {
+            DiskNumber = 1
+            DriveLetter = 'I'
+            DependsOn = '[xWaitForDisk]InstallDisk'
+        }
+
+        
+        xSQLServerSetup SQL
+        {
+            InstanceName =  'AutomationJouney'
+            SourcePath = "C:\Software"
+            SourceFolder = "SQL2014"
+            Features= "SQLENGINE,SSMS"
+            #SecurityMode="SQL"
+            DependsOn = '[xDisk]Installdisk'
+            UpdateEnabled = 'True'
+            UpdateSource = 'C:\Software\SQL2014\Updates'
+
+            SetupCredential = $InstallerServiceAccount
+        }
+    }
+}
+
+$SecurePassword = ConvertTo-SecureString -String "Foxtrot.12" -AsPlainText -Force
+$InstallerServiceAccount = New-Object System.Management.Automation.PSCredential ("TLAB\SQLSVC", $SecurePassword)
+$LocalSystemAccount = New-Object System.Management.Automation.PSCredential ("SYSTEM", $SecurePassword)
+
+$ConfigurationData = @{
+    AllNodes = @(
+        @{
+            NodeName="*"
+            PSDscAllowPlainTextPassword=$true
+         }
+        @{
+            NodeName="Server02"
+         }
+    )
+}
+
+SQLInstall -ConfigurationData $ConfigurationData
+Set-DscLocalConfigurationManager -Path .\SQLInstall -Verbose
+Start-DscConfiguration -Path .\SQLInstall -Verbose -Wait -Force
