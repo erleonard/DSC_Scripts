36e0d137f4b9385abec0c787fad8eea2d6503757
erleonard <erleonard@outlook.com>
2015-08-27 10:22:03 -0400

    small fixes

--- SimpleHotfixInstall.ps1
+++ SimpleHotfixInstall.ps1
@@ -1,14 +1,34 @@
 ﻿Configuration UpdateWindowsWithPath 
-{ 
-    Import-DscResource -ModuleName xWindowsUpdate       
-    Node ‘Server02’ 
-    {  
+{
+    Param(
+        [string]$ComputerName='localhost'
+    )
+
+    Import-DscResource -ModuleName xWindowsUpdate, xPendingReboot
+
+    Node $ComputerName
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
         xHotfix HotfixInstall 
         { 
             Ensure = "Present" 
-            Path = "C:\e87e8d5d85abf29e67\x64-Windows8.1-KB2934520-x64.msu" 
+            Path = "C:\Software\x64-Windows8.1-KB2934520-x64.msu" 
             Id = "KB2934520" 
-        }  
-    }  
+        }
+        
+        xPendingReboot RebootCheck1
+        {
+            Name = "RebootCheck1"
+            DependsOn = '[xHotfix]HotfixInstall'
+        }
+    }
 } 
-  
+
+Set-Location C:\Scripts
+UpdateWindowsWithPath
+Set-DscLocalConfigurationManager -Path .\UpdateWindowsWithPath -Verbose 
+Start-DscConfiguration -Verbose -wait -Path .\UpdateWindowsWithPath -Force
