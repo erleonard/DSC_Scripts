87b2b38e899464980901f9c3a5d7391daa1a20de
erleonard <erleonard@outlook.com>
2015-08-30 07:02:52 -0400

    Set Windows Server 2012 R2 Core
    used to downgrade from a minimal interface or full GUI to CORE

---
+++ SetServerCore.ps1
@@ -0,0 +1,39 @@
+﻿Configuration SetServerCore
+{
+    Param(
+        [string]$ComputerName='localhost'
+    )
+
+    Import-DscResource -ModuleName xPendingReboot
+
+    Node $ComputerName
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature RemoveServerGUIShell
+        {
+            Ensure = 'Absent'
+            Name = 'Server-Gui-Shell'
+        }
+
+        WindowsFeature RemoveGuiMgmtInfra
+        {
+            Ensure = 'Absent'
+            Name = 'Server-Gui-Mgmt-Infra'
+        }
+        
+        xPendingReboot RebootCheck1
+        {
+            Name = "RebootCheck1"
+            DependsOn = '[WindowsFeature]RemoveServerGUIShell','[WindowsFeature]RemoveGuiMgmtInfra'
+        }
+    }
+} 
+
+Set-Location C:\Scripts
+SetServerCore
+Set-DscLocalConfigurationManager -Path .\SetServerCore -Verbose 
+Start-DscConfiguration -Verbose -wait -Path .\SetServerCore -Force
