58596cc027d5892325956035509e5948b6dd594f
erleonard <erleonard@outlook.com>
2015-08-30 07:02:14 -0400

    Set Windows Server 2012 R2 Minimal Server Interface

---
+++ SetMinimalServerInterface.ps1
@@ -0,0 +1,33 @@
+﻿Configuration SetMinimalServerInterface 
+{
+    Param(
+        [string]$ComputerName='localhost'
+    )
+
+    Import-DscResource -ModuleName xPendingReboot
+
+    Node $ComputerName
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature MinimalServerInterface
+        {
+            Ensure = 'Absent'
+            Name = 'Server-Gui-Shell'
+        }
+        
+        xPendingReboot RebootCheck1
+        {
+            Name = "RebootCheck1"
+            DependsOn = '[WindowsFeature]MinimalServerInterface'
+        }
+    }
+} 
+
+Set-Location C:\Scripts
+SetMinimalServerInterface
+Set-DscLocalConfigurationManager -Path .\SetMinimalServerInterface -Verbose 
+Start-DscConfiguration -Verbose -wait -Path .\SetMinimalServerInterface -Force
