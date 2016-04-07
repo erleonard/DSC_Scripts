8f2c8671bee43a53b1e1ed670483d00ee23d53f1
erleonard <erleonard@outlook.com>
2016-04-07 13:32:40 -0400

    Setup of HTTPS Pull Server (v2)
    Setup of HTTPS Pull Server (v2). requires powershell 5.0

---
+++ CreatePullServer.ps1
@@ -0,0 +1,56 @@
+﻿#requires -Version 5 -Modules xPSDesiredStateConfiguration
+
+configuration CreatePullServer
+{ 
+    param  
+    ( 
+            [string[]]$NodeName = 'localhost', 
+
+            [ValidateNotNullOrEmpty()] 
+            [string] $certificateThumbPrint,
+
+            [Parameter(Mandatory)]
+            [ValidateNotNullOrEmpty()]
+            [string] $RegistrationKey 
+     ) 
+
+
+     Import-DSCResource -ModuleName xPSDesiredStateConfiguration 
+
+     Node $NodeName 
+     { 
+         WindowsFeature DSCServiceFeature 
+         { 
+             Ensure = "Present" 
+             Name   = "DSC-Service"             
+         } 
+
+
+         xDscWebService PSDSCPullServer 
+         { 
+             Ensure                  = "Present" 
+             EndpointName            = "PSDSCPullServer" 
+             Port                    = 8080 
+             PhysicalPath            = "$env:SystemDrive\inetpub\PSDSCPullServer" 
+             CertificateThumbPrint   = $certificateThumbPrint          
+             ModulePath              = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules" 
+             ConfigurationPath       = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"             
+             State                   = "Started" 
+             DependsOn               = "[WindowsFeature]DSCServiceFeature"                         
+         } 
+
+        File RegistrationKeyFile
+        {
+            Ensure          ='Present'
+            Type            = 'File'
+            DestinationPath = "$env:ProgramFiles\WindowsPowerShell\DscService\RegistrationKeys.txt"
+            Contents        = $RegistrationKey
+        }
+    }
+}
+
+$certificateThumbPrint = Get-ChildItem -path cert:\LocalMachine\My | Where-Object {$_.Subject -match "$env:COMPUTERNAME"}
+
+CreatePullServer -Nodename DSCPULL.tlab.local -CertificateThumbPrint $certificateThumbPrint.Thumbprint -RegistrationKey (new-guid).Guid
+
+Start-DscConfiguration -ComputerName DSCPULL.tlab.local -path '.\CreatePullServer'  -Force -Wait -Verbose
