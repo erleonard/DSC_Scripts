76217c0a2180f52447c41d5b2e3e975992d52c78
erleonard <erleonard@outlook.com>
2016-04-07 13:33:10 -0400

    Configure LCM 2.0 client
    Configure LCM 2.0 client. requires powershell 5.0

---
+++ SetPullClient.ps1
@@ -0,0 +1,41 @@
+﻿#requires -Version 5
+
+[DSCLocalConfigurationManager()]
+configuration PullClientConfigID
+{
+    param  
+        ( 
+            [string[]]$NodeName = 'localhost', 
+
+            [ValidateNotNullOrEmpty()] 
+            [string] $RegistrationKey
+        )
+    Node $NodeName
+    {
+        Settings
+        {
+            RefreshMode = 'Pull'
+            RefreshFrequencyMins = 30 
+            RebootNodeIfNeeded = $true
+        }
+
+        ConfigurationRepositoryWeb DSCPullServerConfig
+        {
+            ServerURL = 'https://DSCPULL.tlab.local:8080/PSDSCPullServer.svc'
+            RegistrationKey = $RegistrationKey
+            ConfigurationNames = @('ADCS01Base')
+        }   
+
+        ReportServerWeb DSCPullServerReport
+        {
+            ServerURL = 'https://DSCPULL.tlab.local:8080/PSDSCPullServer.svc'
+            RegistrationKey = $RegistrationKey
+        }
+    }
+}
+
+$RegistrationKey = Get-Content -Path $env:ProgramFiles\WindowsPowerShell\DscService\RegistrationKeys.txt
+
+PullClientConfigID -NodeName ADCS01.tlab.local -RegistrationKey $RegistrationKey -OutputPath c:\DSC\TargetNodes
+
+Set-DscLocalConfigurationManager -ComputerName ADCS01.tlab.local -path '.\TargetNodes'  -Force -Verbose
